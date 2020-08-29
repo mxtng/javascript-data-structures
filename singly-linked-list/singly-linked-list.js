45a6f9bbed53436eaf3e8ddfa0bc9c70d68353b3
Melvin Tang <mxcodeyard@gmail.com>
2020-08-29 13:02:39 +1000

    add method added

--- singly-linked-list/singly-linked-list.js
+++ singly-linked-list/singly-linked-list.js
@@ -1,14 +1,34 @@
+// singly linked list
+
+// methods -> descriptions
+// ===================================
+// add -> add new node to end of list
+// ===================================
+
 class Node {
-	constructor(value) {
-		this.value = value;
+	constructor(data) {
+		this.data = data;
 		this.next = null;
 	}
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  } 
+class SinglyLinkedList {
+	constructor() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	add(data) {
+		const newNode = new Node(data);
+
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = this.head;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+		this.length++;
+	}
 }
