f4c8667c15fe2466317f158c8c8f89557cc84270
Melvin Tang <mxcodeyard@gmail.com>
2020-08-30 05:56:30 +1000

    add method added (DLL)

--- doubly-linked-list/doubly-linked-list.js
+++ doubly-linked-list/doubly-linked-list.js
@@ -1,16 +1,17 @@
 // doubly linked list
 
-// methods
-// ===============
-// add
-// remove
-// insert
-// reverse
-// ===============
+// method -> description
+// ===================================
+// add -> add new node to end of list
+// get -> retrieve node by index
+// set -> replace node data by index
+// remove -> remove node by index
+// reverse -> reverse list
+// ===================================
 
 class Node {
-	constructor(value) {
-		this.value = value;
+	constructor(data) {
+		this.data = data;
 		this.next = null;
 		this.previous = null;
 	}
@@ -20,5 +21,19 @@ class DoublyLinkedList {
 	constructor() {
 		this.head = null;
 		this.tail = null;
+		this.length = 0;
+	}
+
+	add(data) {
+		const newNode = new Node(data);
+		if (!!this.length) {
+			this.head = newNode;
+		} else {
+			this.tail.next = newNode;
+			newNode.prev = this.tail;
+		}
+
+		this.tail = newNode;
+		this.length++;
 	}
 }
